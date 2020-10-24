ffda04f9a50a91968e11e6aeab0d73710a947a10
drkkgy <drkkgy@gmail.com>
2020-10-24 23:44:34 +0530

    fixed the issue in hyperdrive code

--- train.py
+++ train.py
@@ -2,19 +2,18 @@ from sklearn.linear_model import LogisticRegression
 import argparse
 import os
 import numpy as np
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
-from azureml.data.dataset_factory import TabularDatasetFactory as tdf
+from azureml.data.dataset_factory import TabularDatasetFactory
+
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-data_url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-## Moved the function above the call to resolve name error 
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -40,22 +39,22 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    return x_df , y_df
+    
+    return x_df, y_df
 
- ### YOUR CODE HERE ###
-
-ds = tdf.from_delimited_files(data_url)
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=0)
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
-
+### YOUR CODE HERE ###
 def main():
+    
+    data_url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    ds = TabularDatasetFactory.from_delimited_files(data_url)
+
+    run = Run.get_context()
+    
+    x, y = clean_data(ds)
+
+    # TODO: Split data into train and test sets.
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=0)
+    
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
